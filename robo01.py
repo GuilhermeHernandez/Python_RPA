1fca155ffc5b87d08316b207258875019131a2c7
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-01 13:32:04 -0300

    Robo_01
    Iniciando nosso estudo em RPA , começamos com alguns exeplos de como retornar determinada cordernada(X,Y) , mover o mouse utilizando cordernada(X,Y) e executar click.

---
+++ robo01.py
@@ -0,0 +1,7 @@
+import pyautogui as p
+
+p.sleep(2)          #Aguarda determinado tempo
+print(p.position())         #Mostra a atual posição do mouse!
+
+#p.moveTo() Move mouse para determinada localização
+p.click(34,530) # Executa apenas 1 click em determinada localização.
