af7949f2d133a969aa791c22e0e479394ac34b63
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-02 17:56:28 -0300

    Robo 03 - Cotação do dolar e envio no email
    --- BIBLIOTECA RPA ---
    
    (NAVEGAÇÃO)
    
    - init(visual_automation=False, chrome_browser=True) > Habilita poder abrir o browser .
    - url('') > Navega p/ local inserido .
    - close() > Fecha janela inicializada
    - read() > Retorna valor do texto ou inteiro.
    
    
    (INTERAÇÃO)
    
    - type('elemento XPath','informação_inseri[enter]') > elemento XPath , insere informação a inserir[ação]
    
    
    (TEMPO)
    
    - wait() > Tempo de espera em segundos.
    
    
    (WINDOWS)
    
    - snap('page','nome_pagina.png') > Tira print da tela .
    
    
    
    ****OBS : Para usar elemento web devemos inspecionar o html , e copiar o elemento XPath .
    
    --- BIBLIOTECA PYAUTOGUI ---
    
    (WINDOWS)
    
    - getActiveWindow() > Retorna janela ativa .
    - maximize() > Maximiza a janela .

--- robo03.py
+++ robo03.py
@@ -1,5 +1,10 @@
 import rpa as r
 import pyautogui as p
+import pandas as pd
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 r.init()
 r.url('https://www.melhorcambio.com/dolar-hoje')
@@ -11,3 +16,30 @@ janela.maximize()
 dolar_comercial = r.read('//*[@id="comercial"]')
 
 janela.close()
+
+# texto do email vazio
+texto_email = dolar_comercial + 'Hoje : ' + str(pd.Timestamp('today'))
+
+# Email remetente , senha , destinatario
+de = 'guilhermehernandezbatista@gmail.com'
+senha = 'isxjkklremuywfyj'
+destinatario = 'familiahernandez0123@gmail.com'
+
+# montando mime
+mensagem = MIMEMultipart()
+mensagem['From'] = de
+mensagem['To'] = destinatario
+mensagem['Subject'] = 'Cotação do Dolar'
+
+# Corpo do E-mail com anexos
+mensagem.attach(MIMEText(texto_email, 'plain'))
+
+# Criando sessão de SMTP para envio do E-mail
+sessao = smtplib.SMTP('smtp.gmail.com', 587)  # Usuario gmail e porta
+sessao.starttls()  # Habilita segurança
+sessao.login(de, senha)  # Login de quem enviara o email
+
+texto = mensagem.as_string()
+
+sessao.sendmail(de, destinatario, texto)
+sessao.quit()
