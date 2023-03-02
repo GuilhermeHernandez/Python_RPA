c4d6995d1f34188e7c1a319c11e158602b6628d6
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-02 16:01:56 -0300

    Add files via upload

--- robo02.py
+++ robo02.py
@@ -1,6 +1,13 @@
 import rpa as r
+import pyautogui as p
 
 r.init()            #Permite rpa inicializar automação
 r.url('https://www.google.com.br/')         #Navega até pagina informada !
 r.wait(2.0)         #Espera em segundos informado
+
+janela = p.getActiveWindow()
+janela.maximize()
+
+r.type('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input','RPA[enter]')          #Interage com o elementos informado de acordo com seu parametro.
+
 
