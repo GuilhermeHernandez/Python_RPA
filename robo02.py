23f8b5ede082c912cfaeeeb6352be3fa9f2ebe0a
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-02 13:50:37 -0300

    Robo 2
    --- BIBLIOTECA RPA ---
    
    (NAVEGAÇÃO)
    
    - init(visual_automation=False, chrome_browser=True) > Habilita poder abrir o browser .
    - url('') > Navega p/ local inserido .
    
    
    (INTERAÇÃO)
    
    - type('elemento XPath','informação_inseri[enter]') > elemento XPath , insere informação a inserir[ação]
    
    
    (TEMPO)
    
    - wait() > Tempo de espera em segundos.
    
    
    
    ****OBS : Para usar elemento web devemos inspecionar o html , e copiar o elemento XPath .
    
    --- BIBLIOTECA PYAUTOGUI ---
    
    (WINDOWS)
    
    - getActiveWindow() > Retorna janela ativa .
    - maximize() > Maximiza a janela .

---
+++ robo02.py
@@ -0,0 +1,6 @@
+import rpa as r
+
+r.init()            #Permite rpa inicializar automação
+r.url('https://www.google.com.br/')         #Navega até pagina informada !
+r.wait(2.0)         #Espera em segundos informado
+
