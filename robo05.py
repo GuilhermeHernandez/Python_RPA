bcbcdeb3147776a6f7ba630931894b720e606dd1
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-03 13:41:38 -0300

    Robo 05 - Extração de dados WEB e manipulação
    --- BIBLIOTECA RPA ---
    
    (NAVEGAÇÃO)
    
    - init(visual_automation=False, chrome_browser=True) > Habilita poder abrir o browser .
    - url('') > Navega p/ local inserido .
    
    
    (INTERAÇÃO)
    
    - type('elemento XPath','informação_inseri[enter]') > elemento XPath , insere informação a inserir[ação]
    - table('','arq_temporario') > Retorna a tabela no arquivo temporario.
    
    
    (TEMPO)
    
    - wait() > Tempo de espera em segundos.
    
    
    (WINDOWS)
    
    - snap('page','nome_pagina.png') > Tira print da tela .
    
    
    
    ****OBS : Para usar elemento web devemos inspecionar o html , e copiar o elemento XPath .
    
    --- BIBLIOTECA PYAUTOGUI ---
    
    (WINDOWS)
    
    - getActiveWindow() > Retorna janela ativa .
    - maximize() > Maximiza a janela . 
    - doubleclick(x=,y=) > Executa doubleclik nas cordenadas fornecidas
    
    
    (IMAGEM)
    
    - locateOnScreen('nome_img.png') > Retorna cordenadas da imagem na pagina atual 
    
    
    --- BIBLIOTECA PANDAS ---
    
    
    - read_csv('arq_csv') > Retorna dados do contendo no arquivo CSV
    - to_csv(r'nome_do_arq',mode = 'a' , index=None, header=True) > Escreve os dados em uma nova tabela csv
    
    
    **** OBS : A leitura de uma tabela se resulme em , ultilizar a biblioteca RPA e fazemos uma extração da tabela para uma tabela temporaria , ao fazer isso vamos ultilizar a biblioteca pandas e fazer a leitura desses dados da tabela temporaria para uma variavel (dados = pd.read_csv('nome_arq_temporario')) , e para finalizarmos vamos enviar para um outro csv que é o principal

--- robo05.py
+++ robo05.py
@@ -1,6 +1,7 @@
 import rpa as r
 import pyautogui as p
 import pandas as pd
+import os as o
 
 r.init()  # Permitindo automatização
 r.url('https://rpachallengeocr.azurewebsites.net/')  # Navega até URL informada
@@ -13,11 +14,25 @@ r.wait(5.0)
 pagina = 1
 
 while pagina <= 3:
-    r.table('//*[@id="tableSandbox"]', 'temp.csv')  # Faz leitura da tabela e armazena em um arquivo temporario
-    dados = pd.read_csv('temp.csv')  # Faz a leitura dados contido no arquivo CSV
-    dados.to_csv(r'WebTable.csv', mode='a', index=None, header=True)  # Escreve os dados em uma nova tabela csv
+    if pagina == 1:
+        r.table('//*[@id="tableSandbox"]', 'temp.csv')  # Faz leitura da tabela e armazena em um arquivo temporario
+        dados = pd.read_csv('temp.csv')  # Faz a leitura dados contido no arquivo CSV
+        dados.to_csv(r'WebTable.csv', mode='a', index=None, header=True)  # Escreve os dados em uma nova tabela csv
 
-    r.click('//*[@id="tableSandbox_next"]')
-    pagina = pagina + 1
+        r.click('//*[@id="tableSandbox_next"]')
+        pagina = pagina + 1
 
-r.close()
+    else:
+        r.table('//*[@id="tableSandbox"]', 'temp.csv')  # Faz leitura da tabela e armazena em um arquivo temporario
+        dados = pd.read_csv('temp.csv')  # Faz a leitura dados contido no arquivo CSV
+        dados.to_csv(r'WebTable.csv', mode='a', index=None, header=False)  # Escreve os dados em uma nova tabela csv
+
+        r.click('//*[@id="tableSandbox_next"]')
+        pagina = pagina + 1
+
+janela.close()
+
+o.remove('temp.csv')    #removendo arquivo temporario
+
+csv_word = pd.read_csv(r'WebTable.csv') #Lendo dados da tabela CSV e armazenando em uma variavel
+csv_word.to_excel('Tabela Web.xlsx')    #convertendo CSV para Excel
