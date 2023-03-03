3e2815916a484858e248e2da1f5c7e9a49f5fe4c
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-03 11:51:05 -0300

    Robo 04 - Aprendendo navegar via imagens.
    --- BIBLIOTECA RPA ---
    
    (NAVEGAÇÃO)
    
    - init(visual_automation=False, chrome_browser=True) > Habilita poder abrir o browser .
    - url('') > Navega p/ local inserido .
    - close() > Fecha janela inicializada
    - read() > Retorna valor do texto ou inteiro.
    
    
    (INTERAÇÃO)
    
    - type('elemento XPath','informação_inseri[enter]') > elemento XPath , insere informação a inserir[ação]
    
    
    (TEMPO)
    
    - wait() > Tempo de espera em segundos.
    
    
    (WINDOWS)
    
    - snap('page','nome_pagina.png') > Tira print da tela .
    
    
    
    ****OBS : Para usar elemento web devemos inspecionar o html , e copiar o elemento XPath .
    
    --- BIBLIOTECA PYAUTOGUI ---
    
    (WINDOWS)
    
    - getActiveWindow() > Retorna janela ativa .
    - maximize() > Maximiza a janela . 
    - doubleclick(x=,y=) > Executa doubleclik nas cordenadas fornecidas
    
    
    (IMAGEM)
    
    - locateOnScreen('nome_img.png') > Retorna cordenadas da imagem na pagina atual

--- robo04.py
+++ robo04.py
@@ -5,23 +5,33 @@ import pyautogui as p
 #mov_google = p.position()
 #print(mov_google)
 
-p.doubleClick(x=35, y=526)
+p.doubleClick(x=35, y=526)  #Duplo click no navegador
 p.sleep(4)
 
-janela = p.getActiveWindow()
-janela.maximize()
+janela = p.getActiveWindow()    #Retorna pagina automatizada
+janela.maximize()   #Mamixa pagina automatizada
 
-p.write('www.udemy.com')
-p.press('enter')
+p.write('www.udemy.com')    #Insere texto para pesquisa
+p.press('enter')    #Comando enter
 p.sleep(6)
 
-indetificando_pesquisa = p.locateOnScreen('Pesquisa.PNG')   #Passando img para reconhecimento
-local_pesquisa = p.center(indetificando_pesquisa)   #Passando centro da imagem
+indentificando_pesquisa = p.locateOnScreen('Pesquisa.PNG')   #Passando img para reconhecimento
+local_pesquisa = p.center(indentificando_pesquisa)   #Passando centro da imagem
 
-p.moveTo(local_pesquisa, duration=1)
-p.click(local_pesquisa)
+p.moveTo(local_pesquisa)    #Movendo mouse para centro da imagem
+p.click(local_pesquisa)     #Clicando no centro da imagem
 p.sleep(2)
 
-p.write('RPA')
+p.write('RPA')      #Inserindo texto para pesquisa
 p.press('enter')
+p.sleep(5)
+
+p.screenshot('Cursos RPA.png')
+p.sleep(2)
+
+indentificando_fechar = p.locateOnScreen('Close.PNG')   #Passando imagem para fechar
+local_fechar = p.center(indentificando_fechar)  #Centro da imagem fecha
+
+p.moveTo(local_fechar)#Movendo até fechar
+p.click(local_fechar)
 p.sleep(2)
